8ccbe5177efbff562a0ec2e383d8adb645c54ce8
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-14 12:56:09 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,3 +1,5 @@
+// assets/js/firebase.js
+// Inicialização Firebase (CDN modular) para GitHub Pages
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
