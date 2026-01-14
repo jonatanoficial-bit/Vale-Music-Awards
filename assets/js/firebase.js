29aad4bdf6e41399ef9bb63f791aaa3cfa18686f
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-14 11:45:51 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,7 +1,7 @@
-// Firebase (CDN modular) – inicialização do app
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCVRe7edXYJsQO7JTNA5ALyKBuEX9FjYGw",
@@ -15,3 +15,4 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
